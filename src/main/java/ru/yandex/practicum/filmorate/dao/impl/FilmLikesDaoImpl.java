70337acd60067bf8c5359e0be0f66a2a20ceca9f
viciossevsk <viciossevsk@gmail.com>
2023-03-09 01:01:24 +0300

    TZ 12. fix2

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmLikesDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmLikesDaoImpl.java
@@ -20,19 +20,23 @@ import static ru.yandex.practicum.filmorate.otherFunction.AddvansedFunctions.str
 @Slf4j
 @Data
 public class FilmLikesDaoImpl implements FilmLikesDao {
-    private final JdbcTemplate jdbcTemplate;
-    private final UserEventDao userEventDao;
     private final static String GET_USER_LIKES_BY_FILM_ID_SQL = "select users_id from film_likes where film_id = ?";
     private final static String SET_NEW_LIKE_TO_FILM_SQL = "insert into film_likes(film_id, users_id) values(?, ?)";
     private final static String DELETE_LIKE_FROM_FILM_SQL = "delete from film_likes where film_id = ? and users_id = ?";
     private final static String DELETE_ALL_LIKES_OF_FILM_SQL = "delete from film_likes where film_id = ?";
     private final static String DELETE_ALL_LIKES_OF_USER_SQL = "delete from film_likes where users_id = ?";
+    private final static String CHECK_EXIST_FILM_LIKE_SQL = "select count(*) from film_likes where " +
+            "users_id = ? and FILM_ID = ?";
+    private final JdbcTemplate jdbcTemplate;
+    private final UserEventDao userEventDao;
 
     @Override
     public void addLikeToFilm(Integer filmId, Integer userId) {
-        jdbcTemplate.update(SET_NEW_LIKE_TO_FILM_SQL, filmId, userId);
-        log.info(stringToGreenColor("record event user log... user id={} set like to film id={}"), userId, filmId);
-        userEventDao.setUserEvent(userId, EventType.LIKE, OperationType.ADD, filmId);
+        if (checkUserLikeExist(userId, filmId)) {
+            jdbcTemplate.update(SET_NEW_LIKE_TO_FILM_SQL, filmId, userId);
+            log.info(stringToGreenColor("record event user log... user id={} set like to film id={}"), userId, filmId);
+            userEventDao.setUserEvent(userId, EventType.LIKE, OperationType.ADD, filmId);
+        }
     }
 
     @Override
@@ -59,5 +63,14 @@ public class FilmLikesDaoImpl implements FilmLikesDao {
 
     private Integer buildUserId(ResultSet rs) throws SQLException {
         return rs.getInt("users_id");
+    }
+
+    public Boolean checkUserLikeExist(Integer userId, Integer filmId) {
+        Integer count = jdbcTemplate.queryForObject(CHECK_EXIST_FILM_LIKE_SQL, Integer.class, userId, filmId);
+        if (count > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
